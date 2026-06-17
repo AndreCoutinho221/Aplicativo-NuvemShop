541fa2ff81715d58722f3c6edabb8cf275700bd0
André <andrefscoutinho@gmail.com>
2026-06-17 15:40:52 -0300

    Correção arquivo HTML

--- nuvemapp/src/main/java/com/andre/nuvemapp/controller/NuvemController.java
+++ nuvemapp/src/main/java/com/andre/nuvemapp/controller/NuvemController.java
@@ -33,20 +33,6 @@ public class NuvemController {
 
     @GetMapping("/home")
     public String homeTemporaria(){
-        return """
-            <!DOCTYPE html>
-            <html lang="pt-BR">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Instalar Integração</title>
-            </head>
-            <body>
-                <button onclick="window.location.href='/integrations/nuvemshop/install'">
-                    Instalar Integração
-                </button>
-            </body>
-            </html>
-            """;
+        return "home";
     }
 }
