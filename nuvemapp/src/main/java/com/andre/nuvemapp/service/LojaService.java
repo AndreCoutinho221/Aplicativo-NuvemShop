849e2156a72814f7f73865ee53c18e95b9400414
André <andrefscoutinho@gmail.com>
2026-06-17 17:19:44 -0300

    Correções finais de variáveis e finalização do fluxo de autenticação NuvemShop, entidade loja mapeada para o banco de dados, salvando informações recebidas pela troca do code

--- nuvemapp/src/main/java/com/andre/nuvemapp/service/LojaService.java
+++ nuvemapp/src/main/java/com/andre/nuvemapp/service/LojaService.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Service
 public class LojaService {
 
-    @Value("${nuvemshop.client-id")
+    @Value("${nuvemshop.app-id}")
     private String clientId;
 
     @Value("${nuvemshop.client-secret}")
@@ -26,20 +26,28 @@ public class LojaService {
     @Autowired
     private LojaRepository lojaRepository;
 
+    public LojaService(RestClient.Builder builder, ObjectMapper objectMapper) {
+        this.restClient = builder.build();
+        this.objectMapper = objectMapper;
+    }
+
     public void trocarCodePorToken(String code, String state){
         Map<String, String> body = Map.of(
                 "client_id", clientId,
                 "client_secret", clientSecret,
-                "grant_type", "autorization_code",
+                "grant_type", "authorization_code",
                 "code", code
         );
-
+        System.out.println(clientId);
+        System.out.println(clientSecret);
         String response = restClient.post()
-                .uri("https://www.nuvemshop.com/apps/authorize/token")
+                .uri("https://www.tiendanube.com/apps/authorize/token")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(body)
                 .retrieve()
                 .body(String.class);
+
+        System.out.println("RESPONSE NUVEMSHOP: " + response);
 
         Map<String, Object> json = objectMapper.readValue(response, Map.class);
 
