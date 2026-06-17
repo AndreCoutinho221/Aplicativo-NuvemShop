1f3f6c4a503c6aa7a09a9172b4f99804ba55d8d2
André <andrefscoutinho@gmail.com>
2026-06-17 15:48:37 -0300

    Home.html alterado para /static

--- nuvemapp/src/main/java/com/andre/nuvemapp/controller/NuvemController.java
+++ nuvemapp/src/main/java/com/andre/nuvemapp/controller/NuvemController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class NuvemController {
@@ -33,6 +34,6 @@ public class NuvemController {
 
     @GetMapping("/casa")
     public String homeTemporaria(){
-        return "home";
+        return "redirect:/home.html";
     }
 }
